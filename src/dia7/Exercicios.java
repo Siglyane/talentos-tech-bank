749a2fcb737989b81a270f55c6d5a11b3a07fd55
Siglyane <siglyane@gmail.com>
2021-07-07 19:04:14 -0300

    Aula 08: Correção da aula 07 e desafio da aula 08

--- src/dia7/Exercicios.java
+++ src/dia7/Exercicios.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class Exercicios {
     public static void main(String[] args) {
-        exercicio5();
+
+        exercicio5Correcao();
     }
 
     public static void validaSystemInInteiro(Scanner in) {
@@ -32,7 +33,7 @@ public class Exercicios {
         System.out.println(Arrays.toString(listaArray));
 
         System.out.println("Em List");
-//        Scanner in = new Scanner(System.in);
+
         List<Integer> lista = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             System.out.println("Digite um número inteiro");
@@ -49,19 +50,16 @@ public class Exercicios {
 //     Escreva um algoritmo que leia números, insira em um array e após mostre a quantidade de números negativos.
         Scanner in = new Scanner(System.in);
         int[] numeros = new int[5];
+        int totalNegativos = 0;
         for (int i = 0; i < 5; i++) {
             System.out.println("Digite um número inteiro, deve informar números positivos e negativos");
             validaSystemInInteiro(in);
             numeros[i] = in.nextInt();
-        }
-        in.close();
-
-        int totalNegativos = 0;
-        for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] < 0) {
                 totalNegativos++;
             }
         }
+        in.close();
 
 //        Exemplo de outra forma de percorrer o código.
 //        for (Integer numero : numeros) {
@@ -137,7 +135,6 @@ public class Exercicios {
 
     }
 
-
     public static void exercicio5() {
 //     Escreva um algoritmo que simula um jogo da forca simples. o usuario precisara adivinhar uma palavra chutando cada
 //     letra em no máximo 10 chutes, caso o usuario acerte a letra o jogo dirá que ele acertou, caso tenha errado, o numero
@@ -146,41 +143,154 @@ public class Exercicios {
         Scanner in = new Scanner(System.in);
         System.out.println("Jogo da Forca");
         System.out.println("Número de tentativas totais: 10");
-        int chutes = 10;
         List<Character> palavra = new ArrayList<>(Arrays.asList('a', 'm', 'o', 'r', 'a'));
-        char[] erros = new char[10];
-        int contador = 0;
-        for (int i = 0; i < 10; i++) {
+        List<Character> tentativas = new ArrayList<>();
+        int erros = 1;
+        int acertos = 0;
+        while (erros < 11) {
             System.out.println("Insira uma letra para tentar adivinhar a palavra:");
             char tentativa = in.next().charAt(0);
-            if (palavra.contains(tentativa)) {
+            if (tentativas.contains(tentativa)) {
+                System.out.println("Você já tentou essa letra");
+            } else if (palavra.contains(tentativa)) {
                 System.out.println("A palavra possui esta letra!");
                 int posicao = palavra.indexOf(tentativa);
                 System.out.println("Uma das posições dessa letra é: " + (posicao + 1));
-                i--;
-                contador++;
-                if (contador > 3) {
-                    System.out.println("Você possui todas as letras");
+                System.out.println("\nLetras erradas que você tentou:" + tentativas);
+                acertos++;
+
+                if (acertos == palavra.size()) {
+                    System.out.println("Parabéns, você ganhou!");
                     break;
                 }
             } else {
-                erros[i] = tentativa;
+                tentativas.add(tentativa);
                 System.out.println("Você errou");
-                System.out.printf("%d/10 tentativas.", (i + 1));
-                System.out.println("\nLetras erradas inseridas:" + Arrays.toString(erros));
+                System.out.printf("%d/10 tentativas.", erros);
+                System.out.println("\nLetras erradas que você tentou:" + tentativas);
+                erros++;
 
             }
 
-
         }
-
+        if (erros >= 10) {
+            System.out.println("Você perdeu!");
+        }
         System.out.println("A palavra era: Amora");
         in.close();
 
 
     }
 
+    public static void desafioPessoal() {
+//      Acrescentei ao desafio do exercicio 5 um desafio pessoal de inserir uma forma randomica de escolher a palavra.
 
+//     Escreva um algoritmo que simula um jogo da forca simples. o usuario precisara adivinhar uma palavra chutando cada
+//     letra em no máximo 10 chutes, caso o usuario acerte a letra o jogo dirá que ele acertou, caso tenha errado, o numero
+//     de tentativas vai diminuindo. Caso o numero de tentativas chegue a 0 o usuário perde.
+
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Jogo da Forca");
+        System.out.println("Número de tentativas totais: 10");
+
+        // Escolhendo uma palavra e separando em letras
+        String[] palavras = {"amora", "banana", "canela", "manga", "morango"};
+        String palavraEscolhida = palavras[random.nextInt(palavras.length)];
+        List<Character> letras = new ArrayList<>();
+        for (char separandoAsLetras : palavraEscolhida.toCharArray()) {
+            if (!letras.contains(separandoAsLetras)) {
+                letras.add(separandoAsLetras);
+            }
+
+        }
+
+        //Validação das tentativas
+        List<Character> tentativas = new ArrayList<>();
+        int erros = 1;
+        int acertos = 0;
+        while (erros < 11) {
+            System.out.println("\nInsira uma letra para tentar adivinhar a palavra:");
+            char tentativa = in.next().charAt(0);
+            if (tentativas.contains(tentativa)) {
+                System.out.println("Você já tentou essa letra");
+            } else if (letras.contains(tentativa)) {
+                System.out.println("A palavra possui esta letra!");
+                int posicao = letras.indexOf(tentativa);
+                System.out.println("Uma das posições dessa letra é: " + (posicao + 1));
+                tentativas.add(tentativa);
+                System.out.printf("\nLetras que já testou: %s", tentativas);
+                acertos++;
+
+                if (acertos == letras.size()) {
+                    System.out.println("Parabéns, você ganhou!");
+                    break;
+                }
+            } else {
+                tentativas.add(tentativa);
+                System.out.println("Você errou");
+                System.out.printf("%d/10 tentativas.", erros);
+                System.out.printf("\nLetras que já testou: %s", tentativas);
+                erros++;
+
+            }
+
+        }
+        if (erros >= 10) {
+            System.out.println("Você perdeu!");
+        }
+        System.out.printf("A palavra era: %s", palavraEscolhida);
+        in.close();
+
+
+    }
+
+    public static void exercicio5Correcao() {
+
+        Scanner sc = new Scanner(System.in);
+        String[] palavra = {"C", "E", "L", "U", "L", "A", "R"};
+        String[] descoberto = {" _ ", " _ ", " _ ", " _ ", " _ ", " _ ", " _ "};
+        List<String> letrasEscolhidas = new ArrayList<>();
+        int totalAcertos = 0;
+        int tentativas = 10;
+        System.out.println("Bem vindo ao JOGO DA FORCA!");
+        while (tentativas > 0) {
+            System.out.println("\nDigite uma letra: ");
+            String letra = sc.next().toUpperCase().trim();
+            int acertos = 0;
+            letrasEscolhidas.add(letra);
+            for (int i = 0; i < palavra.length; i++) {
+                if (letra.equals(palavra[i])) {
+                    descoberto[i] = letra;
+                    acertos++;
+                    totalAcertos++;
+                }
+            }
+            System.out.println("");
+            for (String l : descoberto) {
+                System.out.print(l + " ");
+            }
+            if (totalAcertos == 7) {
+                System.out.println("\nParabéns! Você acertou a palavra!");
+                break;
+            }
+            if (acertos > 0) {
+                System.out.printf("\nVocê teve %d acerto(s)!\n", acertos);
+            } else {
+                System.out.printf("\nA letra %s não faz parte da palavra!", letra);
+                tentativas--;
+            }
+            System.out.printf("\nVocê ainda tem %d tentativa(s)!", tentativas);
+            System.out.println("\nLetras que já foram escolhidas: ");
+            System.out.println(letrasEscolhidas);
+            System.out.println("\n*********************************");
+        }
+        sc.close();
+    }
 }
+
+
+
 
 
